53f28dd2b75cd7580792fb3e346ec0886975efa3
vevellima <qlstyleinfo@gmail.com>
2022-05-02 18:01:43 -0300

    Conclusao do projeto

---
+++ etapas.js
@@ -0,0 +1,68 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulana Boazinha',
+                partido: 'PA - Partido dos Anjinhos',
+                fotos:[
+                    {url:'vereador01.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrana do Mal',
+                partido: 'PP - Partido dos Pilantras',
+                fotos:[
+                    {url:'vereador02.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '43333',
+                nome: 'Ciclano  Cilada',
+                partido: 'PC - Partido dos Cafagestes',
+                fotos:[
+                    {url:'vereador03.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '38',
+                nome: 'Bondade',
+                partido: 'PA',
+                vice: 'Anjinho',
+                fotos:[
+                    {url:'candidato01.jpg', legenda: 'Prefeito'},
+                    {url:'vice01.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '77',
+                nome: 'Pilantrona',
+                partido: 'PP',
+                vice: 'A Pior',
+                fotos:[
+                    {url:'candidato02.jpg', legenda: 'Prefeito'},
+                    {url:'vice02.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '43',
+                nome: 'Malencarado',
+                partido: 'PC',
+                vice: 'Cafetão',
+                fotos:[
+                    {url:'candidato03.jpg', legenda: 'Prefeito'},
+                    {url:'vice03.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
